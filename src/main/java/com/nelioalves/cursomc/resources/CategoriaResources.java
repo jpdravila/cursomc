2e2e0d02cff02d2b10bc9017e0e98d3815ef9e98
João Pedro de Ávila <jpdravila@gmail.com>
2022-03-23 20:46:46 -0400

    novo teste/teste browser

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
@@ -1,17 +1,26 @@
 package com.nelioalves.cursomc.resources;
 
+import com.nelioalves.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST está funcionando";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
-    /*Estudei o vídeo REST testado então o novo a ser estudado é o
-    >Testando a primeira classe de dominio-Categoria*/
 }
