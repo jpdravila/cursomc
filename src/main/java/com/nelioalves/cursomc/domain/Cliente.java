584665c3e3de1873376bb83bc8b3269422ac6204
João Pedro de Ávila <jpdravila@gmail.com>
2022-04-04 06:40:19 -0400

    Cliente, TipoClientes, telefones e endereços

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -2,20 +2,27 @@ package com.nelioalves.cursomc.domain;
 
 import com.nelioalves.cursomc.domain.enums.TipoCliente;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.*;
 
+@Entity
 public class Cliente implements Serializable {
     public static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
     private String cpfOuCnpj;
     private Integer tipo;
 
+    @OneToMany(mappedBy = "cliente")
     private List<Endereco> enderecos = new ArrayList<>();
 
+    @ElementCollection
+    @CollectionTable(name="TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
     public Cliente(){
