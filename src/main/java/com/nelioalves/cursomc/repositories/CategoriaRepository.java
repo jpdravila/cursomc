81a9a814e581510cf0aa4a875dbfc09769cabb2e
João Pedro de Ávila <jpdravila@gmail.com>
2022-04-15 17:15:58 -0400

    Busca de produtos por nome e categorias

--- src/main/java/com/nelioalves/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/CategoriaRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import com.nelioalves.cursomc.domain.Categoria;
 
+import java.util.List;
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
