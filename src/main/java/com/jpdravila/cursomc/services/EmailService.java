3ba61f2ad59d0f0da6ebe8faae2462afc97c8f35
João Pedro de Ávila <jpdravila@gmail.com>
2022-04-27 13:17:06 -0400

    Email HTML

--- src/main/java/com/jpdravila/cursomc/services/EmailService.java
+++ src/main/java/com/jpdravila/cursomc/services/EmailService.java
@@ -3,9 +3,14 @@ package com.jpdravila.cursomc.services;
 import com.jpdravila.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 }
