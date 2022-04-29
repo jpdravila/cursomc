27f0f17eba61984013bae46807940433622f18ed
João Pedro de Ávila <jpdravila@gmail.com>
2022-04-29 05:29:08 -0400

    Adicionando senha ao cliente.

--- src/main/java/com/jpdravila/cursomc/services/DBService.java
+++ src/main/java/com/jpdravila/cursomc/services/DBService.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import com.jpdravila.cursomc.domain.enums.EstadoPagamento;
 import com.jpdravila.cursomc.domain.enums.TipoCliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.jpdravila.cursomc.domain.Categoria;
@@ -33,6 +34,8 @@ import com.jpdravila.cursomc.repositories.ProdutoRepository;
 @Service
 public class DBService {
 
+    @Autowired
+    private BCryptPasswordEncoder pe;
     @Autowired
     private CategoriaRepository categoriaRepository;
     @Autowired
@@ -111,7 +114,7 @@ public class DBService {
         estadoRepository.saveAll(Arrays.asList(est1, est2));
         cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-        Cliente cli1 = new Cliente(null, "Maria Silva", "jpdravila@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+        Cliente cli1 = new Cliente(null, "Maria Silva", "jpdravila@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 
         cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
